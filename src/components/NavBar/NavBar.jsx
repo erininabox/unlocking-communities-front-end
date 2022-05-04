ce241a4e44401c796c40a41e7babade56cd3122f
sarahpicard <sarahannrey225@gmail.com>
2022-05-04 14:29:34 -0400

    add image for login view

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,30 +7,31 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <nav>
           <ul className={styles.ul}>
-            {/* <li><Link to="/profiles">Profiles</Link></li> */}
+            <li><Link to="/profiles">Profiles</Link></li>
             <li className={styles.logout}>
               <button className={styles.btn}>
-                <Link to="" onClick={handleLogout} style={{ textDecoration: 'none'}}>
-                  LOG OUT
+                <Link to="" onClick={handleLogout} style={{ textDecoration: 'none', color: 'black'}}>
+                  Log Out
                 </Link>
               </button>
               </li>
-            {/* <li><Link to="/changePassword">Change Password</Link></li> */}
+            <li><Link to="/changePassword">Change Password</Link></li>
           </ul>
+          <h1 className={styles.h1}>Debloke Kominote Yo</h1>
         </nav>
         :
         <nav>
           <ul>
-            <li className={styles.logout}>
+            {/* <li className={styles.logout}>
               <button className={styles.btn}>
                 <Link to="/login" style={{ textDecoration: 'none', color: 'black'}}>Log In</Link>
               </button>
-            </li>
-            <li className={styles.logout}>
+            </li> */}
+            {/* <li className={styles.logout}>
               <button className={styles.btn}>
                 <Link to="/signup" style={{ textDecoration: 'none', color: 'black'}}>Sign Up</Link>
               </button>
-            </li>
+            </li> */}
           </ul>
         </nav>
       }
