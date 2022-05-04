cb5aafd374bd78c0f022781423f0474689f94d9a
sarahpicard <sarahannrey225@gmail.com>
2022-05-04 12:50:50 -0400

    adjust navigation view

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,36 @@
 import { Link } from 'react-router-dom'
+import styles from './NavBar.module.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
         <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
+          <ul className={styles.ul}>
+            {/* <li><Link to="/profiles">Profiles</Link></li> */}
+            <li className={styles.logout}>
+              <button className={styles.btn}>
+                <Link to="" onClick={handleLogout} style={{ textDecoration: 'none'}}>
+                  LOG OUT
+                </Link>
+              </button>
+              </li>
+            {/* <li><Link to="/changePassword">Change Password</Link></li> */}
           </ul>
         </nav>
         :
         <nav>
           <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li className={styles.logout}>
+              <button className={styles.btn}>
+                <Link to="/login" style={{ textDecoration: 'none', color: 'black'}}>Log In</Link>
+              </button>
+            </li>
+            <li className={styles.logout}>
+              <button className={styles.btn}>
+                <Link to="/signup" style={{ textDecoration: 'none', color: 'black'}}>Sign Up</Link>
+              </button>
+            </li>
           </ul>
         </nav>
       }
