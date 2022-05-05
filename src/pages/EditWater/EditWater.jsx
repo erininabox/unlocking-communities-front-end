8c61061cdb98a35a91586a73cf2a7e37d05be817
sarahpicard <sarahannrey225@gmail.com>
2022-05-05 13:05:53 -0400

    adjust edit water form

--- src/pages/EditWater/EditWater.jsx
+++ src/pages/EditWater/EditWater.jsx
@@ -1,22 +1,47 @@
 import { GiDroplets } from 'react-icons/gi';
 import { FaPencilAlt } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import styles from './EditWater.module.css';
+import * as inventoryService from '../../services/inventoryService'
 
 const EditWater = () => {
     // State
-    const [inStockNum, setInStockNum] = useState({
-        inStockNum: 0,
+    // const [inStockNum, setInStockNum] = useState({
+    //     inStockNum: 0,
+    // })
+    const [waterStock, setWaterStock] = useState({})
+    const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        name: 'water',
+        waterNum: '',
+        waterToOrder: '',
     })
 
-    const onChangeInStock = (e) => {
-        setInStockNum({ inStockNum: e.target.value })
+    const handleChange = e => {
+        setFormData({
+            ...formData, [e.target.name]: e.target.value
+        })
     }
+
+    // const onChangeInStock = (e) => {
+    //     setInStockNum({ inStockNum: e.target.value })
+    // }
     const handleSubmit = async evt => {
         evt.preventDefault();
-        //Send this off to the db
+        try {
+            await inventoryService.addInventory(formData)
+            .then(formData => {
+                setWaterStock(formData)
+                navigate('/inventory')
+            })
+        } catch (err) {
+            console.log(err)
+            throw (err)
+        }
     }
+
+    const { name, waterNum, waterToOrder } = formData
 
     return (
         <div>
@@ -29,14 +54,32 @@ const EditWater = () => {
                     {/* <GiDroplets></GiDroplets>  */}
                     <button className={styles.editbtn}><FaPencilAlt></FaPencilAlt></button>
                 </p>
-                <p className={styles.inputline}>Quantity In-Stock: <input type="number" onClick={onChangeInStock} className={styles.numberInput}></input></p>
-                <p className={styles.inputline}>Quantity to Order: <input type="number" className={styles.numberInput}></input></p>
+                <p className={styles.inputline}>Quantity In-Stock: 
+                    <input 
+                        type="number"  
+                        className={styles.numberInput}
+                        value={waterNum}
+                        name='waterNum'
+                        onChange={handleChange}
+                    >
+                    </input>
+                </p>
+                <p className={styles.inputline}>Quantity to Order: 
+                    <input 
+                        type="number" 
+                        className={styles.numberInput}
+                        value={waterToOrder}
+                        name="waterToOrder"
+                        onChange={handleChange}
+                    >
+                    </input>
+                </p>
                 <hr />
             </div>
 
             <div className={styles.btnblock}>
-                <Link to="/"><button type="submit" className={styles.btn}>Save</button></Link>
-                <Link to="/"><button type="reset" className={styles.btn}>Cancel</button></Link>
+                <button type="submit" className={styles.btn} onClick={handleSubmit}>Save</button>
+                <button type="reset" className={styles.btn} onClick={() => navigate('/inventory')}>Cancel</button>
             </div>
 
             </form>
