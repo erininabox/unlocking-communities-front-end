f65e0bc8357ee715f80a553ba922e8cea4d814fd
sarahpicard <sarahannrey225@gmail.com>
2022-05-05 10:34:59 -0400

    start notification bar adjustment

--- src/components/NotificationBar/NotificationBar.jsx
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -4,19 +4,19 @@ import { FaBell, FaAngleRight } from "react-icons/fa";
 const NotificationBar = (props) => {
     return (
         <div>
-        <div className={styles.container}>
-        <FaBell></FaBell>
-        <h1>Notifications</h1>
-        {props.notifications.map(notification => {
-            return (
-                <div className={styles.item}
-                key={props.notifications.indexOf(notification)}>
-                        {notification}
-                </div>
-            )
-        })}
-        <FaAngleRight></FaAngleRight>
-        </div>
+            <div className={styles.container}>
+                <FaBell className={styles.bell}/>
+                <h1>Notifications</h1>
+                {props.notifications.map(notification => {
+                    return (
+                        <div className={styles.item}
+                            key={props.notifications.indexOf(notification)}>
+                            {notification}
+                        </div>
+                    )
+                })}
+                <FaAngleRight className={styles.angle}/>
+            </div>
         </div>
     )
 }
