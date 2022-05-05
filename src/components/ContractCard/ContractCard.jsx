137e6d7a80f27093e60f945908b61c45d2fd55f0
sarahpicard <sarahannrey225@gmail.com>
2022-05-05 09:22:29 -0400

    reformat past sales page

--- src/components/ContractCard/ContractCard.jsx
+++ src/components/ContractCard/ContractCard.jsx
@@ -1,12 +1,44 @@
+import styles from './ContractCard.module.css'
+// import { Form } from 'react-bootstrap';
+// import { Button } from 'react-bootstrap';
+// import { Col } from 'react-bootstrap';
+// import { Row } from 'react-bootstrap';
+import { ListGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const ContractCard = ({contract}) => {
   return (
-    <div>
-      <p>Sale Date: {contract.saleDate}</p>
-      <p>Sold By: {contract.entName}</p>
-      <p>Sold To: {contract.buyerFirstName} {contract.buyerLastName}</p>
-      <p>Product: {contract.productPurchased}</p>
-      <p>Paid {contract.amountToBePaid === contract.totalPaid ? '✅ ' : '❌'}</p>
+    <div className={styles.whole}>
+      <ul className={styles.ul}>
+        <li>Date {contract.saleDate}</li>
+        <li>Paid {contract.amountToBePaid === contract.totalPaid ? '✅ ' : '❌'}</li>
+        <li> Product: {contract.productPurchased}</li>
+        <li>Sold By: {contract.entName}</li>
+        <li>Sold To: {contract.buyerFirstName} {contract.buyerLastName}</li>
+      </ul>
     </div>
+      //   <div className={styles.whole}>
+      //   <ListGroup className='align-items-center'>
+      //     <ListGroup.Item
+      //       as="li"
+      //       className="d-flex align-items-center"
+      //     >
+      //       <div className='ms-2 me-auto list-item-group'>
+      //         <div className="fw-bold">Date {contract.saleDate}</div>
+      //         Paid {contract.amountToBePaid === contract.totalPaid ? '✅ ' : '❌'}
+      //         <div>
+      //           Product: {contract.productPurchased}
+      //         </div>
+      //         <div>
+      //           Sold By: {contract.entName}
+      //         </div>
+      //         <div>
+      //           Sold To: {contract.buyerFirstName} {contract.buyerLastName}
+      //         </div>
+      //       </div>
+      //     </ListGroup.Item>
+      //   </ListGroup>
+      // </div>
   )
 }
 
